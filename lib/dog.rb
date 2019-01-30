7ea74520dadeb3f0b25ab250a963bd704b8afac6
Alexandra Palermo <alexmpalermo@gmail.com>
2019-01-30 14:52:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,10 +14,15 @@ end
     new_array = [] 
    @@all.each do |dogs|
      new_array << dogs.name 
+<<<<<<< HEAD
    end
      new_array.each do |names|
        puts names
   end
+=======
+     puts new_array
+  end 
+>>>>>>> 493f9c02214ce91af01f7b438ce85397e66f75ed
 end
   
   def self.clear_all
